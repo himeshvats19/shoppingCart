b1385b108166543055bd27a9a6a94ee5169870d7
ICF2033248 <>
2020-03-26 18:55:03 +0530

    Updates

---
+++ precache-manifest.b9ccefad9fd2f35e3e58443ab674637e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7974db15589a463819ccecd9866b26f",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "502a49250642d118424c",
+    "url": "/shoppingCart/static/css/main.968e1065.chunk.css"
+  },
+  {
+    "revision": "4c79cdee0de41f0d03e8",
+    "url": "/shoppingCart/static/js/2.678b89bf.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/shoppingCart/static/js/2.678b89bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502a49250642d118424c",
+    "url": "/shoppingCart/static/js/main.b7c72bec.chunk.js"
+  },
+  {
+    "revision": "5733e14f7098272f7ff9",
+    "url": "/shoppingCart/static/js/runtime-main.8b307f2b.js"
+  }
+]);
